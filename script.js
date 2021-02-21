5ad588908a7b4202e9984493d8748bf892a40c31
Arthur Sita Gomes <ansitagomes@gmail.com>
2021-02-21 20:35:23 +0100

    JS v1.0 undone

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ Refrigerante/água - 1000ml por pessoa >+ 6 horas 1500ml
 
 Crianças valem por 0,5
 */
+
+function result() {
+    let adult = document.querySelector('#adult')
+    let kids = document.querySelector('#kid')
+    let time = document.querySelector('#time')
+    let hidden = document.querySelector('#ninja')
+    hidden.style.display = 'block'
+
+    if (hidden.style.display == 'none') {
+
+    }
+
+
+}
