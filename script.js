e0200aed1980c659c4499c8e4e0918fcb231f2bd
Arthur Sita Gomes <ansitagomes@gmail.com>
2021-02-21 14:06:42 +0100

    CSS base complete

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Carne - 400gr por pessoa >+ 6 horas - 650gr
+
+Cerveja - 1200ml por pessoa >+ 6 horas - 2000ml
+
+Refrigerante/água - 1000ml por pessoa >+ 6 horas 1500ml
+
+Crianças valem por 0,5
+*/
