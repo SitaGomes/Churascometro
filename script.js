01ada906fb204cd93b821aa488de59c02a958206
Arthur Sita Gomes <ansitagomes@gmail.com>
2021-02-22 11:50:20 +0100

    JS v2.0 Kids introduced

--- script.js
+++ script.js
@@ -13,23 +13,47 @@ function result() {
     let time = document.querySelector('#time')
     let hidden = document.querySelector('#ninja')
 
+    //* Grabbing only the value of the inputs
     let adultConvert = adult.value
-    let timeConvert = +time.value
-    let kidsConvert = +kids.value
+    let kidsConvert = kids.value //! One kid eats half 
 
+    let meatB4 = 400
+    let meatAfter = 650
+    let beerB4 = 400
+    let beerAfter = 650
+    let cokeB4 = 400
+    let cokeAfter = 650
 
-    //! 
+    //* Final result variables
+    let carne
+    let cerveja
+    let coke
+
+    //! calculating the food, drinks 
+    if (kidsConvert > 0) {
+
+        if (time.value >= 6) {
+            carne = (meatAfter / 2) * kidsConvert
+            coke = (cokeAfter / 2) * kidsConvert
+
+        } else {
+            carne = (meatB4 / 2) * kidsConvert
+            coke = (cokeB4 / 2) * kidsConvert
+
+        }
+    }
+
     if (time.value >= 6) {
-        var carne = 650 * adultConvert
-        var cerveja = 2000 * adultConvert
-        var coke = 1500 * adultConvert
+        carne = + meatAfter * adultConvert
+        cerveja = beerAfter * adultConvert
+        coke = + cokeAfter * adultConvert
 
     } else {
-        carne = 400 * adultConvert
-        cerveja = 1200 * adultConvert
-        coke = 1000 * adultConvert
-
+        carne = + meatB4 * adultConvert
+        cerveja = beerB4 * adultConvert
+        coke = + cokeB4 * adultConvert
     }
+
 
     var screen = document.querySelector('#product')
 
